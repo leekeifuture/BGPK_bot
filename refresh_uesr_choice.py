8c28e4fab9e6b6dff31d72e617cc52c33864180b
leekeifuture <opnoe09@gmail.com>
2018-08-23 23:03:44 +0300

    Added path to database

--- refresh_uesr_choice.py
+++ refresh_uesr_choice.py
@@ -2,11 +2,12 @@
 #!/usr/bin/python3.6
 
 
+from constants import path
 from functions import sql_execute
 
 
 def main():
-    sql_execute('Bot_db', 'DELETE FROM user_choice')
+    sql_execute(path + 'Bot_db', 'DELETE FROM user_choice')
 
 
 if __name__ == '__main__':
