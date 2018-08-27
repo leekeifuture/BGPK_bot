22f3f448383f937d0702599e3b348aefcf9af1b4
leekeifuture <opnoe09@gmail.com>
2018-08-27 11:08:11 +0300

    Defined os environ variables

--- config.py
+++ config.py
@@ -2,16 +2,19 @@
 #!/usr/bin/python3.6
 
 
-my_id = ''  # my id
+from os import environ
 
-token = ''  # bot token
 
-WEBHOOK_HOST = ''  # IP adress
+my_id = environ['my_telegram_id']  # my id
+
+token = environ['bot_token']  # bot token
+
+WEBHOOK_HOST = environ['server_ip']  # IP adress
 WEBHOOK_PORT = 443  # 443, 80, 88, 8443
 WEBHOOK_LISTEN = '0.0.0.0'
 
-WEBHOOK_SSL_CERT = ''  # path to certificate
-WEBHOOK_SSL_PRIV = ''  # path to private key
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # path to certificate
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # path to private key
 
 WEBHOOK_URL_BASE = 'https://%s:%s' % (WEBHOOK_HOST, WEBHOOK_PORT)
 WEBHOOK_URL_PATH = '/%s/' % token
