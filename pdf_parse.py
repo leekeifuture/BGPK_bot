575bc70a6203265c9d3281c0e1d9a9a18790e91a
leekeifuture <opnoe09@gmail.com>
2018-08-27 21:11:19 +0300

    Some PEP8 and imports corrections

--- pdf_parse.py
+++ pdf_parse.py
@@ -2,16 +2,16 @@
 #!/usr/bin/python3.6
 
 
-import os
-import sys
-import config
-import telebot
-import hashlib
+from os import walk
+import config as conf
+from hashlib import md5
+from sys import exc_info
 from urllib import request
+from telebot import TeleBot
 from datetime import datetime
 
 
-bot = telebot.TeleBot(config.token)
+bot = TeleBot(conf.token)
 
 raspisanie = 'http://www.bspc.brest.by/files/raspisanie/'
 currently_pdf_dir = '/home/ubuntu/bot/PDF/'
@@ -21,10 +21,10 @@ pdfs = [
     's_1k.pdf', 'r_1k.pdf', 'm_1k.pdf', 'u_1k.pdf', 's_2k.pdf', 'r_2k.pdf',
     'm_2k.pdf', 'u_2k.pdf', 's_3k.pdf', 'r_3k.pdf', 'm_3k.pdf', 'u_3k.pdf',
     's_4k.pdf', 'r_4k.pdf', 'm_4k.pdf'
-]  # u_4k.pdf
+]
 
-tree = os.walk(currently_pdf_dir)
-tree_site_dir = os.walk(currently_pdf_from_site)
+tree = walk(currently_pdf_dir)
+tree_site_dir = walk(currently_pdf_from_site)
 
 
 for name_of_pdf_file in pdfs:
@@ -34,9 +34,9 @@ for name_of_pdf_file in pdfs:
         request.urlretrieve(my_file, my_dir)
     except:
         print('\n\n' + str(datetime.now())
-              [:-7] + ' | ' + str(sys.exc_info()[1]) + '\n\n')
-        bot.send_message(config.my_id, str(datetime.now())[
-                         :-7] + ' | PDF (' + name_of_pdf_file[:4] + ') | ' + str(sys.exc_info()[1]))
+              [:-7] + ' | ' + str(exc_info()[1]) + '\n\n')
+        bot.send_message(conf.my_id, str(datetime.now())[
+                         :-7] + ' | PDF (' + name_of_pdf_file[:4] + ') | ' + str(exc_info()[1]))
         continue
 
 tree_hash = []
@@ -45,7 +45,7 @@ for files_names in tree:
         with open(currently_pdf_dir + fn, 'rb') as file:
             file_name = file.read()
             file.close()
-        h = hashlib.md5(file_name).hexdigest()
+        h = md5(file_name).hexdigest()
         tree_hash.append(h)
 
 ind = -1
@@ -56,7 +56,7 @@ for files_names in tree_site_dir:
         with open(currently_pdf_from_site + fn, 'rb') as file:
             file_name = file.read()
             file.close()
-        h = hashlib.md5(file_name).hexdigest()
+        h = md5(file_name).hexdigest()
         if h not in tree_hash:
-            bot.send_message(config.my_id, str(datetime.now())[
+            bot.send_message(conf.my_id, str(datetime.now())[
                              :-7] + ' | ' + fn[:4] + ' | ' + tree_hash[ind] + ' >> ' + h)
