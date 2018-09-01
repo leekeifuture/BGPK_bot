fdf8de2d84a3e65a90ea0265b60b5891662ca3c2
leekeifuture <opnoe09@gmail.com>
2018-09-01 10:14:12 +0300

    Added "select_teacher" condition

--- refresh_uesr_choice.py
+++ refresh_uesr_choice.py
@@ -10,7 +10,7 @@ def main():
     sql_con = connect(path + 'Bot.db')
     cursor = sql_con.cursor()
     cursor.execute('''DELETE FROM user_choice
-    					    WHERE step = "select_teacher"''')
+                            WHERE step = "select_teacher"''')
     sql_con.commit()
     cursor.close()
     sql_con.close()
